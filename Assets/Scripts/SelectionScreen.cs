83b6d0e5a1ead68096d107ea6697a7b4ce902ec4
terminal29 <20899505+terminal29@users.noreply.github.com>
2020-07-02 16:38:21 +1000

    Added build, itchio pictures

--- Assets/Scripts/SelectionScreen.cs
+++ Assets/Scripts/SelectionScreen.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Unity.Mathematics;
-using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
